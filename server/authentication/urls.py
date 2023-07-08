9b2937aea4296180f5b824378a8bf5304ffcfe8e
Mahoud-orabi <mahmoudorabi462002@gmail.com>
2023-07-08 18:35:20 +0300

    modified files and change models to login with email instead username

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
-from .views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-
-urlpatterns =[
+urlpatterns = [
     path('register/',views.RegisterView.as_view(),name='register'),
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
